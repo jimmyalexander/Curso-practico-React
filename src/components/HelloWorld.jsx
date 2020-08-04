b3db97abbbe00f8e06406505ac9b3d90f0c4fd06
jimmy alexander castiblanco <jkastiblanco@gmail.com>
2020-08-04 18:39:02 -0500

    como ultimo añadimos ESLint

---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import '../assets/styles/App.scss';
+const HelloWorld = () =>(
+        <div>Hola Mundo</div>
+);
+        
+export default HelloWorld;                              

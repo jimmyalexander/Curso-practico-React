e7777906d8d7158516025cf5cec6ecfabfe13ebb
jimmy alexander castiblanco <jkastiblanco@gmail.com>
2020-08-06 18:29:05 -0500

    finalizamos nuestros estilos

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Categories = ({ children }) => (
+const Categories = ({ children, title }) => (
   <div className='categories'>
-    <h3 className='categories__title'>Mi lista</h3>
+    <h3 className='categories__title'>{title}</h3>
     {children}
   </div>
 );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import logo from '../assets/static/logo-platzi-video-BW2.png';
+import UserIcon from '../assets/static/user-icon.png';
 
 const Header = () => (
   <header className='header'>
-    <img className='header__img' src='' alt='Platzi Video' />
+    <img className='header__img' src={logo} alt='Platzi Video' />
     <div className='header__menu'>
       <div className='header__menu--profile'>
-        <img src='' alt='' />
+        <img src={UserIcon} alt='' />
         <p>Perfil</p>
       </div>
       <ul>

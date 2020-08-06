33a127e5d97cf3a23b7afd1d82b23c0967ad2b53
jimmy alexander castiblanco <jkastiblanco@gmail.com>
2020-08-06 17:23:20 -0500

    estructurando platzi video

--- src/components/HelloWorld.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import '../assets/styles/App.scss';
-const HelloWorld = () =>(
-        <div>Hola Mundo</div>
-);
-        
-export default HelloWorld;                              
